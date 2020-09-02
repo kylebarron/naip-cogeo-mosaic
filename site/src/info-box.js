9cc22b0e618bcf460f529995243eed8a1708612a
Kyle Barron <kylebarron2@gmail.com>
2020-09-02 11:03:54 -0600

    Use accordion for info box

--- site/src/info-box.js
+++ site/src/info-box.js
@@ -1,9 +1,57 @@
 import React from "react";
-import { Header, Select, Icon } from "semantic-ui-react";
+import { Accordion, Select, Icon } from "semantic-ui-react";
 import { mosaicOptions } from "./constants";
 
 export default function InfoBox(props) {
   const { mosaicYearRange, onChange, zoomIn } = props;
+
+  const panels = [
+    {
+      key: "header",
+      title: "Serverless High-Res Imagery",
+      content: {
+        content: (
+          <p>
+            Serverless high-resolution (up to 0.6m){" "}
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://www.fsa.usda.gov/programs-and-services/aerial-photography/imagery-programs/naip-imagery/"
+            >
+              NAIP
+            </a>{" "}
+            map tiles, generated on demand from an{" "}
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://registry.opendata.aws/naip/"
+            >
+              AWS public dataset
+            </a>{" "}
+            of Cloud-Optimized GeoTIFFs.
+            <br />
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://github.com/kylebarron/naip-cogeo-mosaic"
+            >
+              <Icon name="github" />
+              Github
+            </a>
+            <br />
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://kylebarron.dev/blog/cog-mosaic/naip"
+            >
+              <Icon name="book" />
+              Blog post
+            </a>
+          </p>
+        ),
+      },
+    },
+  ];
 
   return (
     <div
@@ -16,40 +64,14 @@ export default function InfoBox(props) {
         padding: 5,
         maxHeight: "70%",
         zIndex: 1,
-        backgroundColor: "#fff",
+        // backgroundColor: "#fff",
         pointerEvents: "auto",
         overflowY: "auto",
         overflow: "visible",
       }}
     >
-      <Header as="h3">Serverless High-Res Imagery</Header>
-      <p>
-        Serverless high-resolution{" "}
-        <a
-          target="_blank"
-          rel="noopener noreferrer"
-          href="https://www.fsa.usda.gov/programs-and-services/aerial-photography/imagery-programs/naip-imagery/"
-        >
-          NAIP
-        </a>{" "}
-        map tiles, generated on demand from an{" "}
-        <a
-          target="_blank"
-          rel="noopener noreferrer"
-          href="https://registry.opendata.aws/naip/"
-        >
-          AWS public dataset
-        </a>{" "}
-        of Cloud-Optimized GeoTIFFs.{" "}
-        <a
-          target="_blank"
-          rel="noopener noreferrer"
-          href="https://github.com/kylebarron/naip-cogeo-mosaic"
-        >
-          <Icon name="github" />
-          Github
-        </a>
-      </p>
+      <Accordion defaultActiveIndex={0} styled panels={panels} />
+      {/* <Header as="h3">Serverless High-Res Imagery</Header> */}
 
       {zoomIn && (
         <p>
